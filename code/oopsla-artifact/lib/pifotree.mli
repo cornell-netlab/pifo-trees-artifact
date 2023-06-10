a5527ae2c19d72a78e62ed7d5942d8a01df5cb03
Anshuman Mohan <anshumanmohan@live.com>
2023-06-10 13:22:21 -0700

    Cut pifotree to match paper

--- code/oopsla-artifact/lib/pifotree.mli
+++ code/oopsla-artifact/lib/pifotree.mli
@@ -1,13 +1,1 @@
-type t =
-  | Leaf of { q : (Packet.meta * Time.t) Pifo.t (* time = pushtime *) }
-  | Node of {
-      q : ((int * Packet.meta) * (Rank.t * Time.t)) Pifo.t;
-      (* int = which of my children to index to. time = pushtime *)
-      kids : t list;
-      istransient : bool;
-    }
-
-val create : Baretree.t -> t
-val random_target : t -> int list
-val istransient : t -> bool
-val anchor_of : int list -> t -> int list
+type t = Leaf of Packet.meta Pifo.t | Internal of (t list * int Pifo.t)
