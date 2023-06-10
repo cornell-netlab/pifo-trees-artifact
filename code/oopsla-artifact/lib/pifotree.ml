c5b93f681979b74040eb2cdbabaf93c679955b46
Anshuman Mohan <anshumanmohan@live.com>
2023-06-10 13:44:13 -0700

    Add pop method

--- code/oopsla-artifact/lib/pifotree.ml
+++ code/oopsla-artifact/lib/pifotree.ml
@@ -1,1 +1,13 @@
+open Util
+
 type t = Leaf of Packet.meta Pifo.t | Internal of (t list * int Pifo.t)
+
+let rec pop (t : t) : (Packet.meta * t) option =
+  match t with
+  | Leaf p ->
+      let* pkt, p' = Pifo.pop p in
+      Some (pkt, Leaf p')
+  | Internal (qs, p) ->
+      let* i, p' = Pifo.pop p in
+      let* pkt, q' = pop (List.nth qs i) in
+      Some (pkt, Internal (replace_nth qs i q', p'))
--- code/oopsla-artifact/lib/pifotree.mli
+++ code/oopsla-artifact/lib/pifotree.mli
@@ -1,1 +1,3 @@
 type t = Leaf of Packet.meta Pifo.t | Internal of (t list * int Pifo.t)
+
+val pop : t -> (Packet.meta * t) option
