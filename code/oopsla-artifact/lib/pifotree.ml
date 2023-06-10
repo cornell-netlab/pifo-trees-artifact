afafc8b5ed7ee61fbe2289ef6b3b0c5a23245555
Anshuman Mohan <anshumanmohan@live.com>
2023-06-10 14:29:32 -0700

    Rescue without redoing PIFO

--- code/oopsla-artifact/lib/pifotree.ml
+++ code/oopsla-artifact/lib/pifotree.ml
@@ -1,22 +1,24 @@
 open Util
 
-type t = Leaf of Packet.t Pifo.t | Internal of (t list * int Pifo.t)
+type t =
+  | Leaf of (Packet.t * Rank.t) Pifo.t
+  | Internal of (t list * (int * Rank.t) Pifo.t)
 
 let rec pop (t : t) : (Packet.t * t) option =
   match t with
   | Leaf p ->
-      let* pkt, p' = Pifo.pop p in
+      let* (pkt, _), p' = Pifo.pop p in
       Some (pkt, Leaf p')
   | Internal (qs, p) ->
-      let* i, p' = Pifo.pop p in
+      let* (i, _), p' = Pifo.pop p in
       let* pkt, q' = pop (List.nth qs i) in
       Some (pkt, Internal (replace_nth qs i q', p'))
 
 let rec push (t : t) (pkt : Packet.t) (path : Path.t) : t =
   match (t, path) with
-  | Leaf p, [ (_, r) ] -> Leaf (Pifo.push p pkt r)
+  | Leaf p, [ (_, r) ] -> Leaf (Pifo.push p (pkt, r))
   | Internal (qs, p), (i, r) :: pt ->
-      let p' = Pifo.push p i r in
+      let p' = Pifo.push p (i, r) in
       let q' = push (List.nth qs i) pkt pt in
       Internal (replace_nth qs i q', p')
   | _ -> failwith "Push: invalid path"
--- code/oopsla-artifact/lib/pifotree.mli
+++ code/oopsla-artifact/lib/pifotree.mli
@@ -1,4 +1,6 @@
-type t = Leaf of Packet.meta Pifo.t | Internal of (t list * int Pifo.t)
+type t =
+  | Leaf of (Packet.t * Rank.t) Pifo.t
+  | Internal of (t list * (int * Rank.t) Pifo.t)
 
 val pop : t -> (Packet.meta * t) option
 val push : t -> Packet.meta -> Path.t -> t
