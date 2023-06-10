931c9c2ecb9da44286d8e4f05fb80614bd6734f9
Anshuman Mohan <anshumanmohan@live.com>
2023-06-10 14:33:52 -0700

    Comment to explain weird path type

--- code/oopsla-artifact/lib/path.ml
+++ code/oopsla-artifact/lib/path.ml
@@ -1,2 +1,9 @@
 type t = (int * Rank.t) list
-(* The foot of this list has a bogus value in the `int` slot. *)
+(* The foot of this list has a bogus value in the `int` slot.
+   We only care about the `rank` of the foot.
+
+   Another way of writing this type is:
+    type t = (int * Rank.t) list * Rank.t
+   where the final `Rank.t` is the singeton foot of the list.
+   However, the existing version is a little easier to work with.
+*)
