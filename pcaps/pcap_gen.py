863412756c1635337e40133fd9384b8b298a0875
Anshuman Mohan <anshumanmohan@live.com>
2023-07-09 12:34:47 -0700

    Black pass

--- pcaps/pcap_gen.py
+++ pcaps/pcap_gen.py
@@ -1,20 +1,20 @@
 from scapy.all import Ether, IP, wrpcap
 
-a = '10:10:10:10:10:10'
-b = '20:20:20:20:20:20'
-c = '30:30:30:30:30:30'
-d = '40:40:40:40:40:40'
-e = '50:50:50:50:50:50'
-f = '60:60:60:60:60:60'
-g = '70:70:70:70:70:70'
-dummy = '1:1:1:1:1:1'
+a = "10:10:10:10:10:10"
+b = "20:20:20:20:20:20"
+c = "30:30:30:30:30:30"
+d = "40:40:40:40:40:40"
+e = "50:50:50:50:50:50"
+f = "60:60:60:60:60:60"
+g = "70:70:70:70:70:70"
+dummy = "1:1:1:1:1:1"
 
 
 def three_flows():
     packets = []
     srcs = [a, b, c] * 3 + [a]
     for src in srcs:
-        packets += Ether(src=src, dst=dummy) / IP(src='1.1.1.1', dst='1.1.1.1')
+        packets += Ether(src=src, dst=dummy) / IP(src="1.1.1.1", dst="1.1.1.1")
     for i, p in enumerate(packets):
         p.time = i / 10
     return packets
@@ -24,7 +24,7 @@ def three_flows_bursty():
     packets = []
     srcs = [a, b, c] * 16 + [a, b]
     for src in srcs:
-        packets += Ether(src=src, dst=dummy) / IP(src='1.1.1.1', dst='1.1.1.1')
+        packets += Ether(src=src, dst=dummy) / IP(src="1.1.1.1", dst="1.1.1.1")
     time = 0
     for i, p in enumerate(packets):
         p.time = (i / 10) + int(i / 5)
@@ -35,7 +35,7 @@ def two_then_three():
     packets = []
     srcs = [b, c] * 7 + [a] * 6 + [a, b, c] * 10
     for src in srcs:
-        packets += Ether(src=src, dst=dummy) / IP(src='1.1.1.1', dst='1.1.1.1')
+        packets += Ether(src=src, dst=dummy) / IP(src="1.1.1.1", dst="1.1.1.1")
     for i, p in enumerate(packets):
         p.time = i / 10
     return packets
@@ -45,7 +45,7 @@ def five_flows():
     packets = []
     srcs = [a, b, c, d, e] * 10
     for src in srcs:
-        packets += Ether(src=src, dst=dummy) / IP(src='1.1.1.1', dst='1.1.1.1')
+        packets += Ether(src=src, dst=dummy) / IP(src="1.1.1.1", dst="1.1.1.1")
     for i, p in enumerate(packets):
         p.time = i / 10
     return packets
@@ -55,19 +55,19 @@ def seven_flows():
     packets = []
     srcs = [a, b, c, d, e, f, g] * 7 + [a]
     for src in srcs:
-        packets += Ether(src=src, dst=dummy) / IP(src='1.1.1.1', dst='1.1.1.1')
+        packets += Ether(src=src, dst=dummy) / IP(src="1.1.1.1", dst="1.1.1.1")
     for i, p in enumerate(packets):
         p.time = i / 10
     return packets
 
 
 def generate_pcaps():
-    wrpcap('./pcaps/three_flows.pcap', three_flows())
-    wrpcap('./pcaps/three_flows_bursty.pcap', three_flows_bursty())
-    wrpcap('./pcaps/two_then_three.pcap', two_then_three())
-    wrpcap('./pcaps/five_flows.pcap', five_flows())
-    wrpcap('./pcaps/seven_flows.pcap', seven_flows())
+    wrpcap("./pcaps/three_flows.pcap", three_flows())
+    wrpcap("./pcaps/three_flows_bursty.pcap", three_flows_bursty())
+    wrpcap("./pcaps/two_then_three.pcap", two_then_three())
+    wrpcap("./pcaps/five_flows.pcap", five_flows())
+    wrpcap("./pcaps/seven_flows.pcap", seven_flows())
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     generate_pcaps()
--- pcaps/plot.py
+++ pcaps/plot.py
@@ -2,47 +2,50 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from matplotlib.patches import Patch
 
-c1 = 'red'
-c2 = 'skyblue'
-c3 = 'forestgreen'
-c4 = 'lightsalmon'
-c5 = 'dodgerblue'
-c6 = 'darkseagreen'
-c7 = 'orchid'
+c1 = "red"
+c2 = "skyblue"
+c3 = "forestgreen"
+c4 = "lightsalmon"
+c5 = "dodgerblue"
+c6 = "darkseagreen"
+c7 = "orchid"
 
 
 def getColor(src):
     prefix = str(src)[0:2]
-    if (prefix == "17"):
+    if prefix == "17":
         return c1
-    elif (prefix == "35"):
+    elif prefix == "35":
         return c2
-    elif (prefix == "52"):
+    elif prefix == "52":
         return c3
-    elif (prefix == "70"):
+    elif prefix == "70":
         return c4
-    elif (prefix == "88"):
+    elif prefix == "88":
         return c5
-    elif (prefix == "10"):
+    elif prefix == "10":
         return c6
-    elif (prefix == "12"):
+    elif prefix == "12":
         return c7
     else:
-        return 'white'
+        return "white"
 
 
-legend_elements_basic = [Patch(color=c1, label='A'),
-                         Patch(color=c2, label='B'),
-                         Patch(color=c3, label='C')]
+legend_elements_basic = [
+    Patch(color=c1, label="A"),
+    Patch(color=c2, label="B"),
+    Patch(color=c3, label="C"),
+]
 
-legend_elements_five = legend_elements_basic + [Patch(color=c5, label='E')]
+legend_elements_five = legend_elements_basic + [Patch(color=c5, label="E")]
 
-legend_elements_seven = legend_elements_five + [Patch(color=c6, label='F'),
-                                                Patch(color=c7, label='G')]
+legend_elements_seven = legend_elements_five + [
+    Patch(color=c6, label="F"),
+    Patch(color=c7, label="G"),
+]
 
 
 def flesh_out_plot(f, df):
-
     # Setting Y-axis limits, ticks
     f.set_ylim(0, len(df))
     f.axes.yaxis.set_visible(False)
@@ -56,8 +59,7 @@ def flesh_out_plot(f, df):
         # (start, stride) in y-axis
         treetime = row["popped"] - row["pushed"]
         color = getColor(row["src"])
-        f.broken_barh([(row["pushed"], treetime)],
-                      (index, 1), facecolors=color)
+        f.broken_barh([(row["pushed"], treetime)], (index, 1), facecolors=color)
     f.invert_yaxis()
 
 
@@ -67,21 +69,35 @@ def make_plot(df, subplt, name):
     df1 = df.sort_values("pushed")
     df1 = df1.reset_index()
     flesh_out_plot(f1, df1)
-    subplt.savefig(name, bbox_inches='tight')
+    subplt.savefig(name, bbox_inches="tight")
 
 
 def plot():
-    for i in ["fcfs", "fcfs_bin", "strict", "strict_bin", "rr", "rr_bin",
-              "wfq", "wfq_bin", "hpfq", "twopol", "twopol_bin", "threepol",
-              "threepol_bin"]:
+    for i in [
+        "fcfs",
+        "fcfs_bin",
+        "strict",
+        "strict_bin",
+        "rr",
+        "rr_bin",
+        "wfq",
+        "wfq_bin",
+        "hpfq",
+        "twopol",
+        "twopol_bin",
+        "threepol",
+        "threepol_bin",
+    ]:
         df = pd.read_csv(f"_build/output{i}.csv")
         make_plot(df, plt, i)
+
 
 def plot_extension():
     for i in ["extension", "extension_ternary"]:
         df = pd.read_csv(f"_build/output{i}.csv")
         make_plot(df, plt, i)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     # plot()
     plot_extension()
