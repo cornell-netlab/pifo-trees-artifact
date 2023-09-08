ae57b42e981cc69ed35aa7aacc717d0ce94d93ed
Anshuman Mohan <anshumanmohan@live.com>
2023-09-08 13:42:16 -0400

    Comment to explain how PIFOs maintain FIFO order in case of rank ties

--- lib/rank.ml
+++ lib/rank.ml
@@ -1,7 +1,10 @@
 type t = float * Time.t
 
 let cmp (r1, t1) (r2, t2) =
-  (* If the float portions are identical, try to break ties using time. *)
+  (* If the float portions are identical, try to break ties using time.
+     Using this cmp function in out PIFOs will give us FIFO order in case
+     of rank ties, which is what we want.
+  *)
   if r1 == r2 then Time.cmp t1 t2 else if r1 -. r2 < 0. then -1 else 1
 
 let create f t = (f, t)
